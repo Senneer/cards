56b4b0a6de628c8d7dfa04867d04c4425ce279f8
Senneer <senneerr@gmail.com>
2017-09-26 01:42:00 +0700

    remade numbers to pseudo-elements, added popstate

--- build/js/main.js
+++ build/js/main.js
@@ -2,49 +2,63 @@
 
 var templateScript = $('#cardTemplate').html();
 
-function addCard(type) {
-  $($('.cards__item')[cards.length - 1]).addClass('_inDeck');
-  cards.push({ 'type': type, 'id': cards.length + 1 });
-  $('.cards').append(template(cards[cards.length - 1]));
-  window.history.pushState('', '', cards[cards.length - 1].id);
+var prevSet = null;
+
+function UpdateUrl(arr) {
+  var path = '';
+  $.each(arr, function (i, item) {
+    if (i !== 0) {
+      path += '%2F' + item.type;
+    } else {
+      path += item.type;
+    }
+  });
+  prevSet = history.state;
+  window.history.pushState(cards, '', path);
 }
 
-cards = cards.map(function (item, i) {
-  item.id = i + 1;
-  return item;
-});
+function AddCard(type) {
+  cards.push({ 'type': type });
+  $('.cards').append(template(cards[cards.length - 1]));
+  UpdateUrl(cards);
+}
+
+function RemoveCard(el, arr) {
+  arr.pop();
+  el.remove();
+  UpdateUrl(arr);
+}
 
 var template = Handlebars.compile(templateScript);
 $.each(cards, function (i) {
   $('.cards').append(template(cards[i]));
 });
-
-var cardsArr = $('.cards__item');
-
-$.each(cardsArr, function (i, item) {
-  if (i < cardsArr.length - 1) {
-    $(item).addClass('_inDeck');
-  }
-});
+UpdateUrl(cards);
 
 $('body').on('click', function (e) {
   if ($('.cards__item').length === 0) {
     if (e.shiftKey && e.altKey) {
-      addCard('wide');
+      AddCard('wide');
     } else if (e.shiftKey) {
-      addCard('narrow');
+      AddCard('narrow');
     }
   }
 });
 
 $(document).on('click', '.cards__item', function (e) {
   if (e.shiftKey && e.altKey) {
-    addCard('wide');
+    AddCard('wide');
   } else if (e.shiftKey) {
-    addCard('narrow');
+    AddCard('narrow');
   } else {
-    $(this).prev().removeClass('_inDeck');
-    $(this).remove();
-    cards.pop();
+    var lastEl = $(this).parent().children().last();
+    RemoveCard(lastEl, cards);
   }
+});
+
+$(window).on('popstate', function () {
+  $('.cards').html('');
+  $.each(history.state, function (i) {
+    $('.cards').append(template(history.state[i]));
+  });
 });
